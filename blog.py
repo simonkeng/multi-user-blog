98f968131edee1813778f7bc036a67c62e6f7f83
Simon Keng <simonkeng7@gmail.com>
2016-11-14 19:44:44 -0800

    changed code to original version

--- blog.py
+++ blog.py
@@ -14,23 +14,16 @@ jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
 #### Steve Huffman's template framework
 #### set up for use with jinja
 
-def render_str(self, template, **params):
-    t = jinja_env.get_template(template)
-    return t.render(params)
-
 class BlogHandler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
     def render_str(self, template, **params):
-        return render_str(template, **params)
+        t = jinja_env.get_template(template)
+        return t.render(params)
 
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
-
-def render_post(response, post):
-    response.out.write('<br>' + post.subject + '</br><br>')
-    response.out.write(post.content)
 
 class MainPage(BlogHandler):
     def get(self):
